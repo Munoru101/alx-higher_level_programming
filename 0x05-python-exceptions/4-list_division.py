035bc86d00a3038e74f5e66608aa94cb4ffd3985
Munoru101 <dennismunoru@gmail.com>
2023-12-20 11:58:21 +0300

    Function for element-wise division between two lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+
+    for i in range(list_length):
+        try:
+            element_1 = my_list_1[i]
+            element_2 = my_list_2[i]
+            result = element_1 / element_2
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            result_list.append(result)
+
+    return result_list
+
+
+result = list_division(my_list_1, my_list_2, list_length)
+print(result)
