061f2a660db3ee0a38b9782b42534e8429d2c396
Munoru101 <dennismunoru@gmail.com>
2024-05-27 10:57:29 +0300

    import_as used

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     """Do the math and print the result"""
-    from calculator_1 import add, sub, mul, div
-
+    import calculator_1 as calc
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(int(a), int(b), int(calc.add(a, b))))
+    print("{} - {} = {}".format(int(a), int(b), int(calc.sub(a, b))))
+    print("{} * {} = {}".format(int(a), int(b), int(calc.mul(a, b))))
+    print("{} / {} = {}".format(int(a), int(b), int(calc.div(a, b))))
