b3821cd35e96e2849db2119972111836a59960af
Munoru101 <dennismunoru@gmail.com>
2024-05-27 11:17:43 +0300

    improved script

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """Do the math and print the result"""
     import calculator_1 as calc
+
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(int(a), int(b), int(calc.add(a, b))))
-    print("{} - {} = {}".format(int(a), int(b), int(calc.sub(a, b))))
-    print("{} * {} = {}".format(int(a), int(b), int(calc.mul(a, b))))
-    print("{} / {} = {}".format(int(a), int(b), int(calc.div(a, b))))
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
