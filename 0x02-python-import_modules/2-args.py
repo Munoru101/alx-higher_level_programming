ae30ea9edb4d308f8e999a41f9f5ba0150b90d54
Munoru101 <dennismunoru@gmail.com>
2024-05-27 11:51:31 +0300

    program with a dynamic script that prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    if num_args > 0:
+        for i, arg in enumerate(args, start=1):
+            print(f"{i}: {arg}")
+
+
+if __name__ == "__main__":
+    main()
